f3620062c305a0055395157adaff5e9c731ac5b8
Koji Shimba <me@shimba.co>
2019-08-16 22:55:36 +0900

    Fix test

--- test/htmlrb_test.rb
+++ test/htmlrb_test.rb
@@ -2,16 +2,30 @@ require "test_helper"
 
 class HtmlrbTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Htmlrb::VERSION
+    refute_nil Htmlrb::VERSION
   end
 
-  def test_it_does_something_useful
-    html = Htmlrb.build do |t|
-      t.div class: "main", alt: "hello" do
-        t.span "hello"
+  def test_it_builds_html
+    actual_html = Htmlrb.build do |t|
+      t.doctype
+      t.html do
+        t.head do
+          t.title "Yuru Camp△"
+        end
+        t.body do
+          t.a href: "http://yurucamp.jp" do
+            "Official site"
+          end
+
+          t.form(method: "POST") do
+            t.input(name: "name", value: "Nadeshiko")
+          end
+        end
       end
     end
 
-    assert_equal "foo", html
+    expected_html = %(<!DOCTYPE html><html><head><title>Yuru Camp△</title></head><body><a href="http://yurucamp.jp">Official site</a><form method="POST"><input name="name" value="Nadeshiko"></form></body></html>)
+
+    assert_equal expected_html, actual_html
   end
 end
